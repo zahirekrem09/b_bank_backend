dd2fb75eab2ebeb79e7a717586283af3d70dcf85
zahirekrem09 <1820980144@stu.adu.edu.tr>
2021-01-23 12:01:38 +0200

    added produc settings

--- authentication/utils.py
+++ authentication/utils.py
@@ -17,4 +17,4 @@ class Util:
     def send_email(data):
         email = EmailMessage(
             subject=data['email_subject'], body=data['email_body'], to=[data['to_email']])
-        # EmailThread(email).start()
+        EmailThread(email).start()
