c8147264fa258332a1258ab20d76662fcec4f05b
zahirekrem09 <1820980144@stu.adu.edu.tr>
2021-01-27 22:54:42 +0200

    added custom permisson

--- ticket/views.py
+++ ticket/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .serializers import (TicketSerializer, TicketClientDetailSerializer)
 from .models import Ticket
 from django.shortcuts import render
-
+from .permission import IsOwnerOrReadOnly
 
 from rest_framework import generics, status, views, permissions
 
@@ -15,5 +15,6 @@ class TicketsView(generics.ListCreateAPIView):
 
 class TicketsDetailView(generics.RetrieveUpdateAPIView):
     serializer_class = TicketClientDetailSerializer
+    permission_classes = (permissions.IsAuthenticated, IsOwnerOrReadOnly)
     queryset = Ticket.objects.all()
     lookup_field = 'id'
