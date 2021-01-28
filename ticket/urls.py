b344745fbc35fc54ac227481ccceb509f24bf28b
ferruhcihan <ferruhcihan20@gmail.com>
2021-01-28 23:53:25 +0300

    updated ticket

--- ticket/urls.py
+++ ticket/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import (TicketsView, TicketsDetailView,)
+from .views import (TicketsView, ClientTicketsDetailView, ConnectorTicketsDetailView)
 
 urlpatterns = [
     path('tickets/', TicketsView.as_view(), name='tickets-list'),
-    path('tickets/<int:id>', TicketsDetailView.as_view(), name='tickets-detail'),
+    path('client-tickets/<int:id>', ClientTicketsDetailView.as_view(), name='client-tickets-detail'),
+    path('connector-tickets/<int:id>', ConnectorTicketsDetailView.as_view(), name='connector-tickets-detail'),
 
 ]
