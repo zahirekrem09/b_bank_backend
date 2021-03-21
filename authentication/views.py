29fb4dbd1e179ca7ced91e3146eea8593948956f
zahirekrem09 <1820980144@stu.adu.edu.tr>
2021-03-21 20:35:15 +0200

    fixed bug

--- authentication/views.py
+++ authentication/views.py
@@ -246,8 +246,10 @@ class LoginAPIView(generics.GenericAPIView):
 
     def post(self, request):
         serializer = self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        if serializer.is_valid(raise_exception=True):
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 """
