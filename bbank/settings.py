ac2be630ab0cf61f65902aa730bc0389303b3f50
zahirekrem09 <1820980144@stu.adu.edu.tr>
2021-03-27 21:48:40 +0200

    bug fixed email send

--- bbank/settings.py
+++ bbank/settings.py
@@ -181,12 +181,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / "media_root"
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
 EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "bbankdummymail@gmail.com"
