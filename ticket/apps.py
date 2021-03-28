158c04575b67a742845f8b102bd8ce42a1103223
zahirekrem09 <1820980144@stu.adu.edu.tr>
2021-03-28 18:47:08 +0300

    update email templates

--- ticket/apps.py
+++ ticket/apps.py
@@ -4,7 +4,7 @@ from django.apps import AppConfig
 class TicketConfig(AppConfig):
     name = 'ticket'
 
-    # def ready(self):
-    #     print("Starting Scheduler ...")
-    #     from .send_mail_scheduler import send_mail
-    #     send_mail.start()
+    def ready(self):
+        print("Starting Scheduler ...")
+        from .send_mail_scheduler import send_mail
+        send_mail.start()
