5fa977acc9a1ab20ba6ca01c05113a22682ae136
zahirekrem09 <1820980144@stu.adu.edu.tr>
2021-04-04 18:39:58 +0300

    bug fixed2

--- ticket/views.py
+++ ticket/views.py
@@ -153,7 +153,7 @@ class ConnectorTicketsDetailView(generics.RetrieveUpdateAPIView):
 
 class ConnectorTicketsIntakeView(generics.RetrieveUpdateAPIView):
     serializer_class = TicketConnectorIntakeSerializer
-    permission_classes = (permissions.IsAuthenticated, )
+    permission_classes = (permissions.IsAuthenticated, IsConnectorUser,)
     queryset = Ticket.objects.all()
     lookup_field = 'id'
 
