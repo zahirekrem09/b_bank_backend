24da36a5c5b58bfdd9815d9004a9b0767783f57e
zahirekrem09 <zhrekrmsrtk@gmail.com>
2021-04-20 11:55:40 +0300

    fixed media url

--- bbank/settings.py
+++ bbank/settings.py
@@ -18,7 +18,8 @@ import django_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -182,10 +183,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
+# MEDIA_URL = '/media/'
+
+# MEDIA_ROOT = BASE_DIR / "media"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
 MEDIA_URL = '/media/'
-
-MEDIA_ROOT = BASE_DIR / "media_root"
-
 
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
