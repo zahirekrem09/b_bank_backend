a28071bc0a85a5a1c02fc261624caf6f92c5af1f
zahirekrem09 <zhrekrmsrtk@gmail.com>
2021-04-29 23:43:24 +0300

    email send error fixed

--- bbank/settings.py
+++ bbank/settings.py
@@ -231,10 +231,15 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = config("EMAIL_HOST")
-EMAIL_PORT = config("EMAIL_PORT")
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+# EMAIL_HOST = config("EMAIL_HOST")
+# EMAIL_PORT = config("EMAIL_PORT")
+# EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+# EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "bbankdummymail@gmail.com"
+EMAIL_HOST_PASSWORD = "bbankbbank12"
 
 
 CORS_ALLOW_ALL_ORIGINS = True
