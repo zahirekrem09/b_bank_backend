065cba7a58c87d81d75aa44d0b88fb656cc6f6f1
zahirekrem09 <zhrekrmsrtk@gmail.com>
2021-05-04 12:46:32 +0300

    fixed email service

--- bbank/settings.py
+++ bbank/settings.py
@@ -231,15 +231,10 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = config("EMAIL_HOST")
-# EMAIL_PORT = config("EMAIL_PORT")
-# EMAIL_HOST_USER = config("EMAIL_HOST_USER")
-# EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
-
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = "bbankdummymail@gmail.com"
-EMAIL_HOST_PASSWORD = "bbankbbank12"
+EMAIL_HOST = os.environ.get("EMAIL_HOST")
+EMAIL_PORT = os.environ.get("EMAIL_PORT")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 
 
 CORS_ALLOW_ALL_ORIGINS = True
